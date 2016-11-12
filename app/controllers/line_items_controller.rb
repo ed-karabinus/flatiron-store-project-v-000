4350a8feddc001a4d12c6d62b478cda933ab5229
ed-karabinus <ejkarabin@gmail.com>
2016-11-11 22:02:02 -0600

    Wrote LineItemsController#create; all checking out while logged in tests are passing and five adding to cart while logged in tests are passing.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,9 @@
 class LineItemsController < ApplicationController
-
+  def create
+    unless current_user.current_cart
+      current_user.current_cart = current_user.carts.build
+    end
+    current_user.current_cart << LineItem.create(item_id: item_id)
+    current_user.current_cart.save
+  end
 end
